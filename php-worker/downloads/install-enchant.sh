18b298abee7aabf72d3c75813d79c5f33f4decba
Luoyy <zishang520@gmail.com>
2021-11-17 12:09:44 +0800

    update

---
+++ php-worker/downloads/install-enchant.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+path() {
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+}
+
+main(){
+    cd /tmp/enchant-1.6.1 && ./configure --prefix=/usr && make -j$(nproc) && make install
+    exit
+}
+path
+main
