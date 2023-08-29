0c52719f154ce8e65eebe430dfdf875d83d730f8
luoyy <zishang520@gmail.com>
2023-08-29 14:50:14 +0800

    修改逻辑

--- socket/startup.sh
+++ socket/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 LOCAL_IP=$(hostname -i | grep -oE '((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])' | head -n 1)
-export INTRANET_IP="${INTRANET_IP:-127.0.0.1}"
+export INTRANET_IP="${LOCAL_IP:-127.0.0.1}"
 
 # Start crond in background
 crond -l 2 -b
 
 # Start socket in foreground
-php "${APP_CODE_PATH_CONTAINER:-/www}/start.php" start
+php start.php start
